303d3fb5514a5eff8e3bde9fa632b7bcdc37740a
Nicholas Easter <nicholas.easter17@gmail.com>
2022-10-06 21:22:25 -0700

    added db_config file for deployment variables

---
+++ backend/src/main/java/com/blog/db_config.java
@@ -0,0 +1,20 @@
+package com.blog;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class db_config {
+    
+    @Bean
+    public DataSource gDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(System.getenv("JAWSDB_URL"));
+        dataSourceBuilder.username(System.getenv("JAWSDB_USERNAME"));
+        dataSourceBuilder.password(System.getenv("JAWSDB_PASSWORD"));
+        return dataSourceBuilder.build();
+    }
+}
