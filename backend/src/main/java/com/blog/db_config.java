e3a35f9d3b1bee8792037779dfcead5d18d0c4f3
NeasterDev <nicholas.easter17@gmail.com>
2023-02-14 13:07:04 -0800

    I am trying a different config file approach that the heroku docs recommend to see if that fixes the login issue i am having

--- backend/src/main/java/com/blog/db_config.java
+++ backend/src/main/java/com/blog/db_config.java
@@ -1,5 +1,9 @@
 package com.blog;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
 import javax.sql.DataSource;
 
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -10,14 +14,25 @@ import org.springframework.context.annotation.Configuration;
 public class db_config {
     
     @Bean
-    public DataSource gDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        System.out.print(System.getenv("JAWSDB_URL"));
-        System.out.print(System.getenv("JAWSDB_USERNAME"));
-        System.out.print(System.getenv("JAWSDB_PASSWORD"));
-        dataSourceBuilder.url("jdbc:" + System.getenv("JAWSDB_URL"));
-        dataSourceBuilder.username(System.getenv("JAWSDB_USERNAME"));
-        dataSourceBuilder.password(System.getenv("JAWSDB_PASSWORD"));
-        return dataSourceBuilder.build();
+    // public DataSource gDataSource() {
+    //     DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    //     System.out.print(System.getenv("JAWSDB_URL"));
+    //     System.out.print(System.getenv("JAWSDB_USERNAME"));
+    //     System.out.print(System.getenv("JAWSDB_PASSWORD"));
+    //     dataSourceBuilder.url("jdbc:mysql://" + System.getenv("JAWSDB_URL"));
+    //     dataSourceBuilder.username(System.getenv("JAWSDB_USERNAME"));
+    //     dataSourceBuilder.password(System.getenv("JAWSDB_PASSWORD"));
+    //     return dataSourceBuilder.build();
+    // }
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+    
+        String username = jdbUri.getUserInfo().split(":")[0];
+        String password = jdbUri.getUserInfo().split(":")[1];
+        String port = String.valueOf(jdbUri.getPort());
+        String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+    
+        return DriverManager.getConnection(jdbUrl, username, password);
     }
+    
 }
