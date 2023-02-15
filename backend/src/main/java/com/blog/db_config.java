8860e0a2fbd97244050560cacb9f95affe1fc3d3
Nicholas Easter <nicholas.easter17@gmail.com>
2023-02-14 18:05:09 -0800

    put the datasource file back to how it was because it didnt fix it

--- backend/src/main/java/com/blog/db_config.java
+++ backend/src/main/java/com/blog/db_config.java
@@ -14,25 +14,25 @@ import org.springframework.context.annotation.Configuration;
 public class db_config {
     
     @Bean
-    // public DataSource gDataSource() {
-    //     DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-    //     System.out.print(System.getenv("JAWSDB_URL"));
-    //     System.out.print(System.getenv("JAWSDB_USERNAME"));
-    //     System.out.print(System.getenv("JAWSDB_PASSWORD"));
-    //     dataSourceBuilder.url("jdbc:mysql://" + System.getenv("JAWSDB_URL"));
-    //     dataSourceBuilder.username(System.getenv("JAWSDB_USERNAME"));
-    //     dataSourceBuilder.password(System.getenv("JAWSDB_PASSWORD"));
-    //     return dataSourceBuilder.build();
-    // }
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
-    
-        String username = jdbUri.getUserInfo().split(":")[0];
-        String password = jdbUri.getUserInfo().split(":")[1];
-        String port = String.valueOf(jdbUri.getPort());
-        String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
-    
-        return DriverManager.getConnection(jdbUrl, username, password);
+    public DataSource gDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        System.out.print(System.getenv("JAWSDB_URL"));
+        System.out.print(System.getenv("JAWSDB_USERNAME"));
+        System.out.print(System.getenv("JAWSDB_PASSWORD"));
+        dataSourceBuilder.url("jdbc:mysql://" + System.getenv("JAWSDB_URL"));
+        dataSourceBuilder.username(System.getenv("JAWSDB_USERNAME"));
+        dataSourceBuilder.password(System.getenv("JAWSDB_PASSWORD"));
+        return dataSourceBuilder.build();
     }
+    // private static Connection getConnection() throws URISyntaxException, SQLException {
+    //     URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+    
+    //     String username = jdbUri.getUserInfo().split(":")[0];
+    //     String password = jdbUri.getUserInfo().split(":")[1];
+    //     String port = String.valueOf(jdbUri.getPort());
+    //     String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+    
+    //     return DriverManager.getConnection(jdbUrl, username, password);
+    // }
     
 }
